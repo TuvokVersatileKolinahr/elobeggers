3c0033f5dc5ed106cf8a006f3a0eccac71274832
Wouter Roosendaal <tuvokki@gmail.com>
2016-10-21 08:20:19 +0200

    Return json on post

--- src/main/java/nl/tuvok/elobeggers/controllers/UserController.java
+++ src/main/java/nl/tuvok/elobeggers/controllers/UserController.java
@@ -10,18 +10,18 @@ public class UserController {
 	public UserController(final UserService userService) {
 		get("/users", (req, res) -> userService.getAllUsers(), json());
 
-		get("/users", new Route() {
-			@Override
-			public Object handle(spark.Request request, spark.Response response) throws Exception {
-				return userService.getAllUsers();
-			}
-		});
+//		get("/users", new Route() {
+//			@Override
+//			public Object handle(spark.Request request, spark.Response response) throws Exception {
+//				return userService.getAllUsers();
+//			}
+//		});
 		post("/user", new Route() {
 			@Override
 			public Object handle(spark.Request request, spark.Response response) throws Exception {
 				return userService.createUser("test", "test@test.nl");
 			}
-		});
+		}, json());
 		// more routes
 	}
 }
