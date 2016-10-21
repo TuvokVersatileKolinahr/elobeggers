f0fb4b1518a40b70ba211dd668eb4213ce3b1d93
Wouter Roosendaal <tuvokki@gmail.com>
2016-10-21 08:17:09 +0200

    Return json on get

--- src/main/java/nl/tuvok/elobeggers/controllers/UserController.java
+++ src/main/java/nl/tuvok/elobeggers/controllers/UserController.java
@@ -1,12 +1,15 @@
 package nl.tuvok.elobeggers.controllers;
 
 import static spark.Spark.*;
+import static nl.tuvok.elobeggers.JsonUtil.*;
 
 import nl.tuvok.elobeggers.services.UserService;
 import spark.Route;
 
 public class UserController {
 	public UserController(final UserService userService) {
+		get("/users", (req, res) -> userService.getAllUsers(), json());
+
 		get("/users", new Route() {
 			@Override
 			public Object handle(spark.Request request, spark.Response response) throws Exception {
