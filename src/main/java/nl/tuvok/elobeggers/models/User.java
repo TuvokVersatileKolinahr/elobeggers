a59a4ad3c2fb4cafcbe3e0b4f8c96fbe5d4f5a69
Wouter Roosendaal <tuvokki@gmail.com>
2016-10-22 08:49:37 +0200

    Change DB-name and collections

--- src/main/java/nl/tuvok/elobeggers/models/User.java
+++ src/main/java/nl/tuvok/elobeggers/models/User.java
@@ -6,8 +6,9 @@ import org.mongodb.morphia.annotations.Id;
 
 @Entity("users")
 public class User {
-    @Id
-    private ObjectId id;
+	@Id
+	private ObjectId id;
+	
 	public String name;
 	public String email;
 }
