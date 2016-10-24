a006468bdcedfa07f5ee8a9fb2543b10bcc723e7
Wouter Roosendaal <wouter.roosendaal@planonsoftware.com>
2016-10-24 09:08:06 +0200

    Fixed build failure and proved the need for CI

---
+++ src/main/java/nl/tuvok/elobeggers/controllers/PlayerController.java
@@ -0,0 +1,11 @@
+package nl.tuvok.elobeggers.controllers;
+
+import nl.tuvok.elobeggers.services.PlayerService;
+
+public class PlayerController {
+
+	public PlayerController(PlayerService playerService) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/main/java/nl/tuvok/elobeggers/services/PlayerService.java
@@ -0,0 +1,11 @@
+package nl.tuvok.elobeggers.services;
+
+import org.mongodb.morphia.Datastore;
+
+public class PlayerService {
+
+	public PlayerService(Datastore datastore) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
