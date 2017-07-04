014aced1bfb0cd5078a22ba8d0ceededa7777be4
Wouter Roosendaal <tuvokki@gmail.com>
2017-07-04 19:07:38 +0200

    run postinstall to generate heroku dist folder

--- dist/out-tsc/client/app/games/games.component.spec.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-/*describe('Component: Games', () => {
-  it('should create an instance', () => {
-    let component = new GamesComponent();
-    expect(component).toBeTruthy();
-  });
-});*/
-//# sourceMappingURL=games.component.spec.js.map
